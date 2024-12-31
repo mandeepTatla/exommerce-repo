45a473c71b9718e323d6262b154f5d226f414834
Mandeep Tatla <mandeeptatla2808@gmail.com>
2024-12-31 14:44:37 +1030

    Updated the item to show in  cart

--- lib/shopify/queries/collection.ts
+++ lib/shopify/queries/collection.ts
@@ -43,7 +43,7 @@ export const getCollectionProductsQuery = /* GraphQL */ `
     $reverse: Boolean
   ) {
     collection(handle: $handle) {
-      products(sortKey: $sortKey, reverse: $reverse, first: 100) {
+      products(sortKey: $sortKey, reverse: $reverse, first: 36) {
         edges {
           node {
             ...product
