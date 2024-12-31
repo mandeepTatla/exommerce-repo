0eab9f98a081c0cb4c4ef98228ae3c1dfd6c55dd
Mandeep Tatla <mandeeptatla2808@gmail.com>
2024-12-31 14:59:11 +1030

    use font awsome

--- components/cart/close-cart.tsx
+++ components/cart/close-cart.tsx
@@ -4,7 +4,7 @@ export default function CloseCart() {
   return (
     <div className="relative flex h-12 w-12 items-center justify-end text-white">
       {/* @ts-ignore */}
-      <FaXmark title="Close cart" className="h-6 w-6" /> as IconType
+      <FaXmark title="Close cart" className="h-6 w-6" />
     </div>
   );
 }
